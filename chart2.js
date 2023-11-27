5697365ffa69087d17d48ff494d845d112bf3131
kotbi mohamed <simojetta2@gmail.com>
2023-11-27 10:20:27 -0800

    bissmillah

---
+++ chart2.js
@@ -0,0 +1,22 @@
+const ctx1 = document.getElementById('myChart2');
+
+new Chart(ctx1, {
+  type: 'doughnut',
+  data:{
+  labels: [
+    'Red',
+    'Blue',
+    'Yellow'
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [300, 50, 100],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+    ],
+    hoverOffset: 4
+  }],}
+});
+
